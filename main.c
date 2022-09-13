ca55c2c8f78961c5cf38ee1b9b206b780288805e
wang <960401828@qq.com>
2022-09-13 19:14:16 +0800

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+//文件操作
+//文件分为：程序文件  数据文件
+
 int main() {
-    printf("Hello, World!\n");
+
+    int a = 10000;
+    FILE* pf = fopen("test.txt","wb");
+
+    fwrite(&a,4,1,pf); //二进制的形式写到文件中
+
+    fclose(pf);
+    pf = NULL;
     return 0;
 }

7abddb9a84eb5448d084c0e02418ff374e5ef00e
wang <960401828@qq.com>
2022-09-14 18:40:12 +0800

    second commit

--- main.c
+++ main.c
@@ -18,15 +18,74 @@
     pf = NULL;
     return 0;
 }*/
+/*
 int main()
 {
 
-    FILE* f = fopen("test.txt","r"); //只读模式
+//    FILE* f = fopen("test.txt","r"); //只读模式
+    FILE* f = fopen("test.txt","r"); //写
     if(f == NULL)
     {
-        printf("%s",strerror(errno));
+        printf("%s\n",strerror(errno));
     }
+    //写文件
+//    fputc('a',f);
+//    fputc('b',f);
+//    fputc('c',f);
+    //读文件
+    printf("%c", fgetc(f));
+    printf("%c", fgetc(f));
+    printf("%c", fgetc(f));
+
 
     fclose(f);
+    f = NULL;
     return 0;
 }
+*/
+
+//FILE* stdin
+//stdout
+//stderr
+
+/*int main()
+{
+
+    int ch = fgetc(stdin);
+    fputc(ch,stdout);
+
+
+}*/
+
+/*int main()
+{
+    char buf[1024] =  {0}; //缓冲区
+    FILE* fp = fopen("test.txt","r");
+    if(fp == NULL)
+    {
+        return 0;
+    }
+
+    //读文件
+    fgets(buf,1024,fp);
+    printf("%s",buf);
+
+
+    fclose(fp);
+    fp = NULL;
+}*/
+//scanf() printf(): 只针对标准输入流和输出流的格式化输入\输入语句
+//fscanf() fprintf(); 是针对所有输入流或者所有输出流的格式化输入输出语句
+//sscanf()  是从字符串中读取字符串的数据
+//sprintf(); 吧格式化的数据 存储到字符串中
+
+
+int main()
+{
+
+
+}
+
+
+
+
